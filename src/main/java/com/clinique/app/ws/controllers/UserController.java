dbc9bbb9d7d9a1f54a95c1902067537d5bc6299d
outiti88 <outiti.ayoub@gmail.com>
2020-11-23 13:01:08 +0100

    project init + UserController

---
+++ src/main/java/com/clinique/app/ws/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.clinique.app.ws.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/users")
+public class UserController {
+
+	@GetMapping
+	public String getUser() {
+		return "get user";
+	}
+	
+	@PostMapping
+	public String createUser() {
+		return "create user";
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user";
+	}
+}
